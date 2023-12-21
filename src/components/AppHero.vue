8b5b884645a196864092a56e6dac4d510f778e40
anto24s96 <anto24s96@gmail.com>
2023-12-21 16:38:44 +0100

    [BONUS] exercise completed

---
+++ src/components/AppHero.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    data() {
+        return {
+            file_name: "obama.jpg"
+        }
+    },
+    methods: {
+        getImagePath(path) {
+            return new URL(path, import.meta.url).href
+        }
+    },
+}
+</script>
+<template lang="">
+    <div>
+        <img :src="getImagePath(`../assets/${file_name}`)" alt="obaaama" width="1000px">
+    </div>
+</template>
+<style lang="">
+    
+</style>
